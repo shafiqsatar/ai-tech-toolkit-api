ed4d203db13bc2151270e478c42fb50994bbe58a
shafiqsatar <sapikadha@gmail.com>
2026-02-22 07:00:34 +0800

    Implement CORS support in generate.js
    Add CORS headers to the response for API requests.

--- api/generate.js
+++ api/generate.js
@@ -1,3 +1,8 @@
+res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+if (req.method === "OPTIONS") return res.status(200).end();
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).send("Method not allowed");
 
